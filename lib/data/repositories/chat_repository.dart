53ad97a7aeceb7aaafb89d96a6978e304abf7f86
SMparth sharma <darkcodeing0@gmail.com>
2025-06-19 09:51:14 +0530

    create send message method

--- lib/data/repositories/chat_repository.dart
+++ lib/data/repositories/chat_repository.dart
@@ -1,9 +1,19 @@
 import 'package:chat_app/data/models/chat_room_model.dart';
+import 'package:chat_app/data/models/message_model.dart';
 import 'package:chat_app/data/service/base_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatRepository extends BaseReposisory {
-  CollectionReference get _chatRooms => firestore.collection('chatRooms');
+  CollectionReference get _chatRooms =>
+      firestore.collection('chatRooms'); //create collection of chatroom
+
+  CollectionReference getChatRoomMessage(String chatRoomId) {
+    return _chatRooms
+        .doc(chatRoomId)
+        .collection(
+          'message',
+        ); //create collection of message in chat room collection
+  }
 
   Future<ChatRoomModel> getOrCreateChatRoom(
     String currentUserId,
@@ -37,5 +47,48 @@ class ChatRepository extends BaseReposisory {
     );
     await _chatRooms.doc(roomId).set(newRoom.toMap());
     return newRoom;
+  }
+
+  Future<void> sendMessage({
+    required String chatRoomId,
+    required String senderId,
+    required String receiverId,
+    required String content,
+    MessageType type = MessageType.text,
+  }) async {
+    //bacth for multiple write operation
+
+    final batch = firestore.batch();
+
+    //get message sub collection
+
+    final messageRef = getChatRoomMessage(chatRoomId);
+    final messageDoc = messageRef.doc();
+
+    //chat message
+
+    final message = MessageModel(
+      id: messageDoc.id,
+      chatRoomId: chatRoomId,
+      senderId: senderId,
+      receiverId: receiverId,
+      content: content,
+      timestamp: Timestamp.now(),
+      readBy: [senderId],
+    );
+
+    //add message to sub collection
+
+    batch.set(messageDoc, message.toMap());
+
+    //update chatroom
+
+    batch.update(_chatRooms.doc(chatRoomId), {
+      'lastMessage': content,
+      'lastMessageSenderId': senderId,
+      'lastMessageTime': message.timestamp,
+    });
+    // commite changes
+    batch.commit();
   }
 }
